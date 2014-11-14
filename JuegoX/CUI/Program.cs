f910157fc8ce9f70af60bdfdfb2c43f93de6d83d
FranzJosephKafka <franciscco@gmail.com>
2014-11-14 16:20:03 +0100

    Cambios para dar cohesion

--- JuegoX/CUI/Program.cs
+++ JuegoX/CUI/Program.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EspasaF.ProgramacionEval1.Juego.Logica;
 
-namespace CUI
+namespace EspasaF.ProgramacionEval1.Juego.Logica.CUI
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola Git");
-            Console.ReadLine();
+            int secreto = Logica.ModeloJuego.SecretoDeLaVida();
+
+            Console.WriteLine("El secreto de la vida es " + secreto);
+            Console.ReadKey();
         }
     }
 }
---
+++ JuegoX/LogicaJuego/Juego.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EspasaF.ProgramacionEval1.Juego.Logica
+{
+    public static class ModeloJuego
+    {
+        public static int SecretoDeLaVida()
+        {
+            return 42;
+        }
+    }
+}
